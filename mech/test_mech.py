0b5e76da483491fdba38e239a02f24c95006c3f2
Mike Kinney <mike.kinney@gmail.com>
2020-02-07 16:26:09 -0800

    disable deprecated warnings for now; mock out the utils.locate function

--- mech/test_mech.py
+++ mech/test_mech.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 
 import mech.command
 import mech.mech
+import mech.vmrun
 
 
 def test_version():
@@ -20,31 +21,30 @@ def test_help():
     assert rc == 0
 
 
-first_dict = {
+mechfile_one_entry = {
     'first': {
         'name':
         'first',
         'box':
         'bento/ubuntu-18.04',
         'box_version':
-        '201912.04.0',
-        'url':
-        'https://vagrantcloud.com/bento/boxes/ubuntu-18.04/'
-        'versions/201912.04.0/providers/vmware_desktop.box'
+        '201912.04.0'
     }
 }
-@patch('mech.utils.load_mechfile', return_value=first_dict)
-def test_mech_list_with_one(mock_load_mechfile, capfd):
+@patch('mech.utils.load_mechfile', return_value=mechfile_one_entry)
+@patch('mech.utils.locate', return_value=None)
+def test_mech_list_with_one(mock_locate, mock_load_mechfile, capfd):
     global_arguments = {'--debug': False}
     m = mech.mech.Mech(arguments=global_arguments)
     list_arguments = {'--detail': False}
     m.list(list_arguments)
     out, _ = capfd.readouterr()
+    mock_locate.assert_called()
     mock_load_mechfile.assert_called()
     assert re.search(r'first\s+notcreated', out, re.MULTILINE)
 
 
-two_dict = {
+mechfile_two_entries = {
     'first': {
         'name':
         'first',
@@ -68,13 +68,15 @@ two_dict = {
         'versions/201912.04.0/providers/vmware_desktop.box'
     }
 }
-@patch('mech.utils.load_mechfile', return_value=two_dict)
-def test_mech_list_with_two(mock_load_mechfile, capfd):
+@patch('mech.utils.load_mechfile', return_value=mechfile_two_entries)
+@patch('mech.utils.locate', return_value=None)
+def test_mech_list_with_two(mock_locate, mock_load_mechfile, capfd):
     global_arguments = {'--debug': False}
     m = mech.mech.Mech(arguments=global_arguments)
     list_arguments = {'--detail': False}
     m.list(list_arguments)
     out, _ = capfd.readouterr()
+    mock_locate.assert_called()
     mock_load_mechfile.assert_called()
     assert re.search(r'first\s+notcreated', out, re.MULTILINE)
     assert re.search(r'second\s+notcreated', out, re.MULTILINE)
