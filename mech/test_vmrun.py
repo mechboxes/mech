96d506b760d9f3984b9f0e4ceac2267063493e97
Mike Kinney <mike.kinney@gmail.com>
2020-02-16 14:52:33 -0800

    set executable/provider in test

--- mech/test_vmrun.py
+++ mech/test_vmrun.py
@@ -10,7 +10,8 @@ import mech.vmrun
 @patch('mech.vmrun.VMrun.vmrun', return_value='')
 def test_vmrun_start(mock_vmrun):
     """Test start method."""
-    vmrun = mech.vmrun.VMrun('/tmp/first/some.vmx', user='auser', password='apass')
+    vmrun = mech.vmrun.VMrun('/tmp/first/some.vmx', user='auser',
+                             password='apass', executable='/tmp/vmrun', provider='ws')
     got = vmrun.start()
     mock_vmrun.assert_called()
     assert got == ''
@@ -23,7 +24,8 @@ def test_vmrun_vmrun(mock_popen):
     attrs = {'communicate.return_value': ('output', 'error')}
     process_mock.configure_mock(**attrs)
     mock_popen.return_value = process_mock
-    vmrun = mech.vmrun.VMrun('/tmp/first/some.vmx', user='auser', password='apass')
+    vmrun = mech.vmrun.VMrun('/tmp/first/some.vmx', user='auser', password='apass',
+                             executable='/tmp/vmrun', provider='ws')
     got = vmrun.vmrun('list', vmrun.vmx_file)
     assert got is None
     mock_popen.assert_called()
